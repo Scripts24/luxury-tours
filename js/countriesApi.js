90e98d7510e8e477d069809812951753d8138253
Patricia Atanasoff <system.informatica365@gmail.com>
2026-06-02 22:42:25 -0300

    Key API fix

--- js/countriesApi.js
+++ js/countriesApi.js
@@ -1,58 +1,88 @@
-let config = {
+var config = {
     cUrl: 'https://api.countrystatecity.in/v1/countries',
-    ckey: 'NHhvOEcyWk50N2Vna3VFTE00bFp3MjFKR0ZEOUhkZlg4RTk1MlJlaA=='
+    ckey: '1c61e423c6218e78e8c189e9619cee8ba389f58f97d5440008a03fb3fcfc70a2'
 }
 
-let countrySelect = document.querySelector('.country')
-let stateSelect = document.querySelector('.state')
+
+var countrySelect = document.querySelector('.country'),
+    stateSelect = document.querySelector('.state'),
+    citySelect = document.querySelector('.city')
+
 
 function loadCountries() {
 
     let apiEndPoint = config.cUrl
 
-    fetch(apiEndPoint, { headers: { "X-CSCAPI-KEY": config.ckey } })
-        .then(Response => Response.json())
-        .then(data => {
-            console.log(data);
+    fetch(apiEndPoint, {headers: {"X-CSCAPI-KEY": config.ckey}})
+    .then(Response => Response.json())
+    .then(data => {
+        // console.log(data);
 
-            data.forEach(country => {
-                const option = document.createElement('option')
-                option.value = country.iso2
-                option.textContent = country.name
-                countrySelect.appendChild(option)
-            })
+        data.forEach(country => {
+            const option = document.createElement('option')
+            option.value = country.iso2
+            option.textContent = country.name 
+            countrySelect.appendChild(option)
         })
-        .catch(error => console.error('Error al cargar países:', error))
+    })
+    .catch(error => console.error('Error loading countries:', error))
 
     stateSelect.disabled = true
-
+    citySelect.disabled = true
     stateSelect.style.pointerEvents = 'none'
-
+    citySelect.style.pointerEvents = 'none'
 }
+
 
 function loadStates() {
     stateSelect.disabled = false
-
+    citySelect.disabled = true
     stateSelect.style.pointerEvents = 'auto'
+    citySelect.style.pointerEvents = 'none'
 
     const selectedCountryCode = countrySelect.value
     // console.log(selectedCountryCode);
-    stateSelect.innerHTML = '<option value="">Ciudad</option>' // limpiar estados existentes
+    stateSelect.innerHTML = '<option value="">Select State</option>' // for clearing the existing states
+    citySelect.innerHTML = '<option value="">Select City</option>' // Clear existing city options
 
-    fetch(`${config.cUrl}/${selectedCountryCode}/states`, { headers: { "X-CSCAPI-KEY": config.ckey } })
-        .then(response => response.json())
-        .then(data => {
-            // console.log(data);
+    fetch(`${config.cUrl}/${selectedCountryCode}/states`, {headers: {"X-CSCAPI-KEY": config.ckey}})
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data);
 
-            data.forEach(state => {
-                const option = document.createElement('option')
-                option.value = state.iso2
-                option.textContent = state.name
-                stateSelect.appendChild(option)
-            })
+        data.forEach(state => {
+            const option = document.createElement('option')
+            option.value = state.iso2
+            option.textContent = state.name 
+            stateSelect.appendChild(option)
         })
-        .catch(error => console.error('Error al cargar ciudades:', error))
+    })
+    .catch(error => console.error('Error loading countries:', error))
 }
 
+
+function loadCities() {
+    citySelect.disabled = false
+    citySelect.style.pointerEvents = 'auto'
+
+    const selectedCountryCode = countrySelect.value
+    const selectedStateCode = stateSelect.value
+    // console.log(selectedCountryCode, selectedStateCode);
+
+    citySelect.innerHTML = '<option value="">Select City</option>' // Clear existing city options
+
+    fetch(`${config.cUrl}/${selectedCountryCode}/states/${selectedStateCode}/cities`, {headers: {"X-CSCAPI-KEY": config.ckey}})
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data);
+
+        data.forEach(city => {
+            const option = document.createElement('option')
+            option.value = city.iso2
+            option.textContent = city.name 
+            citySelect.appendChild(option)
+        })
+    })
+}
 
 window.onload = loadCountries
